d665a89e18ab486e12e1f209d451cc096d81df06
Sissy Candice <GoddessServant0@gmail.com>
2018-01-31 15:35:20 -0500

    Server.js cleanup

--- server.js
+++ server.js
@@ -3,26 +3,25 @@ const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const db             = require('./config/database');
 const app            = express();
-const port = 5001;
+const port           = 5001;
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
-MongoClient.connect(db.url, (err, database) => {
+
+MongoClient.connect(db.url, function(err, database) {
     if (err) return console.log(err);
     require('./app/routes/index.js')(app, database.db('node-project'));
-    app.listen(port, () => {
+    app.listen(port, function() {
         console.log('We are live on ' + port);
     });
 });
-
 
 app.use(session({
     secret: 'i need more beers',
     resave: false,
     saveUninitialized: false,
     store: new MongoStore({
-        url: db.url,
+        url: db.url
     })
 }));
