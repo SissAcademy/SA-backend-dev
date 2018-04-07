b0250e81fe897d32c4492f5ad5c08cf960267637
Sissy Emma <emma.thesissy@gmail.com>
2018-04-07 16:24:05 +0200

    I believe I understood, opening at deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -x
 
+openssl aes-256-cbc -K $encrypted_0eb8cab50dbb_key -iv $encrypted_0eb8cab50dbb_iv -in deploy-key.enc -out deploy-key -d
+
 pwd
 ls
 
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 .travis/deploy-key # Allow read access to the private key
-ssh-add .travis/deploy-key # Add the private key to SSH
+chmod 600 deploy-key # Allow read access to the private key
+ssh-add deploy-key # Add the private key to SSH
 
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh -o "StrictHostKeyChecking no" $DEPLOY_USER@$BACKEND_SERVER <<EOF
--- scripts/setDeployKey.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -x
-
-openssl aes-256-cbc -K $encrypted_0eb8cab50dbb_key -iv $encrypted_0eb8cab50dbb_iv -in deploy-key.enc -out deploy-key -d
-cp deploy-key the-key
-
-pwd
-ls
