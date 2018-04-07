055c2dc7ce9e749947cf97e8941ae785e49dcc13
Sissy Emma <emma.thesissy@gmail.com>
2018-04-07 16:49:50 +0200

    Fixed versions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,7 @@ ssh-add /tmp/deploy_rsa # Add the private key to SSH
 q
 ssh -o "StrictHostKeyChecking no" $DEPLOY_USER@$BACKEND_SERVER <<EOF
   cd SA-backend-dev/
+  git checkout -- .
   git checkout do-deploy
   git pull
   npm install
