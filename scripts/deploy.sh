6e59390c1d7e5f836618b3d53f04eff1406fb151
Sissy Emma <emma.thesissy@gmail.com>
2018-04-07 16:08:47 +0200

    Fixed deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 .travis/id_rsa # Allow read access to the private key
-ssh-add .travis/id_rsa # Add the private key to SSH
-
-# git config --global push.default matching
-# git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
-# git push deploy master
+chmod 600 deploy-key # Allow read access to the private key
+ssh-add deploy-key # Add the private key to SSH
 
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh -o "StrictHostKeyChecking no" $DEPLOY_USER@$BACKEND_SERVER <<EOF
