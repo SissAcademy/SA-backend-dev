272318e12a6345e31dcc62f07cc236b4145ff01b
Sissy Emma <emma.thesissy@gmail.com>
2018-04-07 15:37:29 +0200

    Added deployment key to Travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-ssh deploy@188.166.52.223 'cd SA-backend/; git checkout do-deploy; git pull; npm install; npm restart;'
+eval "$(ssh-agent -s)" # Start ssh-agent cache
+chmod 600 .travis/id_rsa # Allow read access to the private key
+ssh-add .travis/id_rsa # Add the private key to SSH
+
+# git config --global push.default matching
+# git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
+# git push deploy master
+
+# Skip this command if you don't need to execute any additional commands after deploying.
+ssh -o "StrictHostKeyChecking no" $DEPLOY_USER@$BACKEND_SERVER <<EOF
+  cd SA-backend/
+  git checkout do-deploy
+  git pull
+  npm install
+  npm restart;
+EOF
