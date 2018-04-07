2d0b8ffbe46d4b4f6c011fdcf3fec1ac0e52233d
Sissy Emma <emma.thesissy@gmail.com>
2018-04-07 16:14:10 +0200

    Just feeling lucky ;-)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 deploy-key # Allow read access to the private key
-ssh-add deploy-key # Add the private key to SSH
+chmod 600 .travis/deploy-key # Allow read access to the private key
+ssh-add .travis/deploy-key # Add the private key to SSH
 
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh -o "StrictHostKeyChecking no" $DEPLOY_USER@$BACKEND_SERVER <<EOF
