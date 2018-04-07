81a7804bde8e791dde7550d6b43631bb65cecb4a
Sissy Emma <emma.thesissy@gmail.com>
2018-04-07 16:40:52 +0200

    Cleaning things a little bit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-set -x
-
-openssl aes-256-cbc -K $encrypted_0eb8cab50dbb_key -iv $encrypted_0eb8cab50dbb_iv -in deploy-key.enc -out deploy-key -d
-
-pwd
-ls
-
+openssl aes-256-cbc -K $encrypted_0eb8cab50dbb_key -iv $encrypted_0eb8cab50dbb_iv -in deploy_rsa.enc -out /tmp/deploy_rsa -d
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 deploy-key # Allow read access to the private key
-ssh-add deploy-key # Add the private key to SSH
-
-# Skip this command if you don't need to execute any additional commands after deploying.
+chmod 600 /tmp/deploy_rsa # Allow read access to the private key
+ssh-add /tmp/deploy_rsa # Add the private key to SSH
+q
 ssh -o "StrictHostKeyChecking no" $DEPLOY_USER@$BACKEND_SERVER <<EOF
   cd SA-backend/
   git checkout do-deploy
