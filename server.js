8ab86bedbece42b4240b503fecf78c0f686180e2
Sissy Emma <emma.thesissy@gmail.com>
2018-04-08 11:03:34 +0200

    server.js file is cleaner now

--- server.js
+++ server.js
@@ -10,13 +10,17 @@ var MongoStore = require('connect-mongo');
 var jwt = require('jsonwebtoken');
 let filters = require('./src/filters');
 
-MongoClient.connect(db.url, function(err, database) {
-    if (err) return console.log(err);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(filters.authentication);
+
+MongoClient.connect(db.url, (err, database) => {
+    if (err) {
+        return console.log(err);
+    }
+
     require('./app/routes/index.js')(app, database.db('node-project'));
-    app.listen(port, function() {
+
+    app.listen(port, () => {
         console.log('We are live on ' + port);
     });
 });
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(filters.authentication);
